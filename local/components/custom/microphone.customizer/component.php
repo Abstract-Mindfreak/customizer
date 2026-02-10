502e528285a2e523cbedd7393819053925b98ec7
Abstract Mindfreak <mungorodnaneve@gmail.com>
2026-02-11 00:48:33 +0300

    Delete local/components/custom/microphone.customizer/component.php

--- local/components/custom/microphone.customizer/component.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-/** @var CBitrixComponent $this */
-/** @var array $arParams */
-/** @var array $arResult */
-$this->executeComponent();
